86b7ce890770216fa2c8f9811a1400af5edbd28a
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-05-06 17:58:39 -0500

    final touches to project, prep for deployment

--- tasktorch/settings.py
+++ tasktorch/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+load_dotenv()
 import os
 import dj_database_url
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('imadeitthroughthebootcampandcouldntbemoreproud')
+SECRET_KEY = os.getenv('imadeitthroughthebootcampandcouldntbemoreproud', "django-insecure-abc123!@#fallbackkey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not 'ON_HEROKU' in os.environ:
